8669480dc5d7390281199f2f9857ee7944818f9b
mudrhiod <mudaranrhiod@gmail.com>
2025-07-19 23:05:54 +0100

    md3 expressive

--- src/scripts/copybutton.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".copy").forEach(copyButton => {
-        copyButton.addEventListener("click", () => {
-            const targetElement = copyButton.dataset.copy;
-        
-            navigator.clipboard.writeText(targetElement).then(() => {
-                
-            });
-        });
-    });
--- src/scripts/dialog.js
+++
@@ -1,9 +0,0 @@
-  const dialogs = document.querySelectorAll(".info-dialog, .preview-dialog");
-
-  dialogs.forEach(dialog => {
-  const openButton = dialog.nextElementSibling;
-  
-  if (openButton) {
-    openButton.addEventListener("click", () => dialog.open = true);
-  }
-});
